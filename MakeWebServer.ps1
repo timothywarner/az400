df548dfc0d253fcfddcb323dae817b94d8833d10
Tim Warner <timothywarner316@gmail.com>
2020-06-27 16:27:54 -0500

    Add Azure Automation scripts

--- MakeWebServer.ps1
+++ MakeWebServer.ps1
@@ -1,24 +1,63 @@
-Configuration MakeWebServer
+configuration MakeWebServer
 {
-    Node "localhost"
+
+Import-DscResource -ModuleName @{ModuleName = 'xWebAdministration';ModuleVersion = '1.19.0.0'}
+Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+    WindowsFeature WebServer
     {
-        WindowsFeature WebServer {
-            Name   = "Web-Server"
-            Ensure = "Present"
-        }
+        Ensure  = 'Present'
+        Name    = 'Web-Server'
+    }
 
-        WindowsFeature ManagementTools {
-            Name   = "Web-Mgmt-Tools"
-            Ensure = "Present"
-        }
+    # IIS Site Default Values
+    xWebSiteDefaults SiteDefaults
+    {
+        ApplyTo                 = 'Machine'
+        LogFormat               = 'IIS'
+        LogDirectory            = 'C:\inetpub\logs\LogFiles'
+        TraceLogDirectory       = 'C:\inetpub\logs\FailedReqLogFiles'
+        DefaultApplicationPool  = 'DefaultAppPool'
+        AllowSubDirConfig       = 'true'
+        DependsOn               = '[WindowsFeature]WebServer'
+    }
 
-        WindowsFeature DefaultDoc {
-            Name   = "Web-Default-Doc"
-            Ensure = "Present"
+    # IIS App Pool Default Values
+    xWebAppPoolDefaults PoolDefaults
+    {
+       ApplyTo               = 'Machine'
+       ManagedRuntimeVersion = 'v4.0'
+       IdentityType          = 'ApplicationPoolIdentity'
+       DependsOn             = '[WindowsFeature]WebServer'
+    }
+
+     File WebContent
+    {
+        Ensure          = "Present"
+        SourcePath      = $SourcePath
+        DestinationPath = $DestinationPath
+        Recurse         = $true
+        Type            = "Directory"
+        DependsOn       = "[WindowsFeature]AspNet45"
+    }
+
+    xWebsite NewWebsite
+    {
+        Ensure          = "Present"
+        Name            = $WebSiteName
+        State           = "Started"
+        PhysicalPath    = $DestinationPath
+        DependsOn       = "[File]WebContent"
+        BindingInfo     = MSFT_xWebBindingInformation
+        {
+            Protocol              = 'https'
+            Port                  = '443'
+            CertificateStoreName  = 'MY'
+            CertificateThumbprint = 'BB84DE3EC423DDDE90C08AB3C5A828692089493C'
+            HostName              = $Website
+            IPAddress             = '*'
+            SSLFlags              = '1'
         }
     }
+
 }
-
-# WebServerConfiguration -OutputPath "C:\DscConfiguration"
-
-# Start-DscConfiguration -Wait -Verbose -Path "C:\DscConfiguration"
---
+++ TestConfig.ps1
@@ -0,0 +1,21 @@
+configuration TestConfig
+{
+    Node IsWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+        }
+    }
+}
