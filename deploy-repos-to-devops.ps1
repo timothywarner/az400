66e4f7d9255a0c7afc0e8945c289b6459ba0b627
Tim Warner <timothywarner316@gmail.com>
2020-12-15 14:10:29 -0600

    Add Docker assets for Dec 2020 delivery

--- deploy-repos-to-devops.ps1
+++
@@ -1,9 +0,0 @@
-# Node Express
-git clone https://github.com/mikepfeiffer/node-express-azure.git
-cd .\node-express-azure
-code .\azuredeploy.json
-
-#ASP.NET Core Demo
-git clone https://github.com/CloudSkills/aspnet-core-demo.git
-cd .\aspnet-core-demo
-
